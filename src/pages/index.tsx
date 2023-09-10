9c3db9b80050c191cdc761c2c1910b0f2f1e8d24
julilan <juli.molnar@outlook.com>
2023-09-10 12:22:59 +0300

    Next image tag, style up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,64 @@
-import { UserButton, useUser, SignOutButton } from "@clerk/nextjs";
+import { UserButton, useUser } from "@clerk/nextjs";
 import Head from "next/head";
-import Link from "next/link";
 
 import { api } from "~/utils/api";
+import type { RouterOutputs } from "~/utils/api";
+
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import Image from "next/image";
+
+dayjs.extend(relativeTime);
+
+const CreatePostWizard = () => {
+  const { user } = useUser();
+
+  console.log(user);
+
+  if (!user) return null;
+
+  return (
+    <div className="flex w-full gap-3">
+      <Image
+        src={user.imageUrl}
+        alt="Profile image"
+        className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
+      />
+      <input
+        placeholder="Type some emojis!"
+        className="grow bg-transparent outline-none"
+      />
+    </div>
+  );
+};
+
+type PostWithUser = RouterOutputs["posts"]["getAll"][number];
+
+const PostView = (props: PostWithUser) => {
+  const { post, author } = props;
+  return (
+    <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
+      <Image
+        src={author.imageUrl}
+        className="h-14 w-14 rounded-full"
+        alt={`@${author.username}'s profile picture`}
+        width={56}
+        height={56}
+      />
+      <div className="flex flex-col">
+        <div className="flex gap-1 text-slate-300">
+          <span>{`@${author.username} `}</span>
+          <span className="font-thin">{` · ${dayjs(
+            post.createdAt,
+          ).fromNow()}`}</span>
+        </div>
+        <span>{post.content}</span>
+      </div>
+    </div>
+  );
+};
 
 export default function Home() {
   const user = useUser();
@@ -23,14 +79,12 @@ export default function Home() {
       <main className="flex h-screen justify-center">
         <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
           <div className="flex border-b border-slate-400 p-4">
-            <UserButton afterSignOutUrl="/" />
-            {!!user.isSignedIn && <SignOutButton />}
+            {!user.isSignedIn && <UserButton afterSignOutUrl="/" />}
+            {user.isSignedIn && <CreatePostWizard />}
           </div>
           <div className="flex flex-col">
-            {[...data, ...data]?.map((post) => (
-              <div key={post.id} className="border-b border-slate-400 p-8">
-                {post.content}
-              </div>
+            {[...data, ...data]?.map((fullPost) => (
+              <PostView {...fullPost} key={fullPost.post.id} />
             ))}
           </div>
         </div>
